c5672de9c686652203b0068840fa50d30e8a8d1b
Jakob Ruckel <jakob@ruckel.de>
2021-05-02 21:05:21 +0200

    Trigger scene load by player entering collider

--- Assets/SceneLoader/SceneLoader.cs
+++ Assets/SceneLoader/SceneLoader.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+
+/**
+ * Usage: Create an empty object, add the SceneLoader component and
+ * a 2D collider.  Set "Target Scene Name" of the SceneLoader
+ * and tick "Is Trigger" for the collider.
+ */
 public class SceneLoader : MonoBehaviour
 {
 
     [SerializeField]
     public string targetSceneName = "";
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(0))
-            TriggerSceneLoad();
-    }
-
     void TriggerSceneLoad() {
         SceneManager.LoadScene(targetSceneName);
+    }
+
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.CompareTag("Player")) {
+            TriggerSceneLoad();
+        }
     }
 }
