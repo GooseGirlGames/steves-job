8dc5c33a8af9abb6773e1b838dc52113bad754bf
YoFionn <ericksoncooperation@gmail.com>
2021-05-02 23:44:01 +0200

    steve can jump now

--- Assets/Player/script/stevecontroller.cs
+++ Assets/Player/script/stevecontroller.cs
@@ -6,12 +6,22 @@ public class stevecontroller : MonoBehaviour {
     public float MovementSpeed = 5;
     public float Jumphight = 3;
 
-    private void Start(){
+    private Rigidbody2D _ridgitbody;
+
+    private void Start()
+    {
+        _ridgitbody = GetComponent<Rigidbody2D>();
     }
 
-    private void Update(){
+    private void Update()
+    {
         var movement = Input.GetAxis("Horizontal");
         Console.WriteLine("movement");
         transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * MovementSpeed;       
+
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(_ridgitbody.velocity.y) < 0.001f)
+        {
+            _ridgitbody.AddForce(new Vector2(0, Jumphight), ForceMode2D.Impulse);
+        }
     }
 }
