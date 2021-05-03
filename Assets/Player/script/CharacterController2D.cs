5f8e5c1936c0c856f1366665a7bc14b50ea52200
YoFionn <ericksoncooperation@gmail.com>
2021-05-03 20:54:00 +0200

    Steve walking animation fixed, Flip is working

--- Assets/Player/script/CharacterController2D.cs
+++ Assets/Player/script/CharacterController2D.cs
@@ -106,7 +106,7 @@ public class CharacterController2D : MonoBehaviour
 			}
 
 			// Move the character by finding the target velocity
-			Vector3 targetVelocity = new Vector2(move * 10f, m_Rigidbody2D.velocity.y);
+			Vector3 targetVelocity = new Vector2(move , m_Rigidbody2D.velocity.y);
 			// And then smoothing it out and applying it to the character
 			m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
 
