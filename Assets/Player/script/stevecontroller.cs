acfc0eef976fc7eca026d04d195779d50f6c9096
YoFionn <ericksoncooperation@gmail.com>
2021-05-04 00:35:29 +0200

    allot of stuff, some animations and the groundcheck specialy

--- Assets/Player/script/stevecontroller.cs
+++ Assets/Player/script/stevecontroller.cs
@@ -12,8 +12,10 @@ public class stevecontroller : MonoBehaviour {
     private bool m_facing_right = true; 
 	private Vector3 m_velocity = Vector3.zero;
    	[Range(0, 1f)] [SerializeField] private float m_movement_smoothing = .05f;	// How much to smooth out the movement
+
     public Animator m_animator;
-    private Rigidbody2D m_ridgitbody;
+    private Rigidbody2D m_rigitbody;
+    public Transform m_ground_check;
 
 //Methoden
 //===================================================================================================================================
@@ -32,8 +34,8 @@ public class stevecontroller : MonoBehaviour {
         // the actual movement
         //Debug.Log(move);
         //Debug.Log(m_ridgitbody.velocity.y);
-        Vector3 player_velocity = new Vector2(move * 10f, m_ridgitbody.velocity.y);
-        m_ridgitbody.velocity = Vector3.SmoothDamp(m_ridgitbody.velocity, player_velocity, ref m_velocity, m_movement_smoothing);
+        Vector3 player_velocity = new Vector2(move * 10f, m_rigitbody.velocity.y);
+        m_rigitbody.velocity = Vector3.SmoothDamp(m_rigitbody.velocity, player_velocity, ref m_velocity, m_movement_smoothing);
 
         if (move < 0 && m_facing_right){
             Flip();
@@ -47,7 +49,7 @@ public class stevecontroller : MonoBehaviour {
 //===================================================================================================================================
 
     private void Start(){
-        m_ridgitbody = GetComponent<Rigidbody2D>();
+        m_rigitbody = GetComponent<Rigidbody2D>();
         m_animator = GetComponent<Animator>();
     }
 
@@ -69,9 +71,9 @@ public class stevecontroller : MonoBehaviour {
         //Debug.Log(Time.fixedDeltaTime);
         //Debug.Log(m_ridgitbody.velocity.y);
 
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(m_ridgitbody.velocity.y) < 0.001f) 
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(m_rigitbody.velocity.y) < 0.001f) 
         {
-            m_ridgitbody.AddForce(new Vector2(0, jump_hight), ForceMode2D.Impulse);
+            m_rigitbody.AddForce(new Vector2(0, jump_hight), ForceMode2D.Impulse);
         }
     }
 }
