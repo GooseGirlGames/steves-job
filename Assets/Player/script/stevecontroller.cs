fa32263bea97d63cdb390518c043cf37952d6ad0
YoFionn <ericksoncooperation@gmail.com>
2021-05-03 15:49:59 +0200

    start implementing flip method

--- Assets/Player/script/stevecontroller.cs
+++ Assets/Player/script/stevecontroller.cs
@@ -14,6 +14,17 @@ public class stevecontroller : MonoBehaviour {
 
     private Rigidbody2D _ridgitbody;
 
+	private void Flip(){
+		// Switch the way the player is labelled as facing.
+		m_FacingRight = !m_FacingRight;
+		// Multiply the player's x local scale by -1.
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
+	}
+
+//===================================================================================================================================
+
     private void Start()
     {
         _ridgitbody = GetComponent<Rigidbody2D>();
