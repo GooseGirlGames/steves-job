c75208be638b518794505054924e592a0bd7a8a5
Jakob Ruckel <jakob@ruckel.de>
2021-05-05 13:39:16 +0200

    Shirt is gone not fortunateley

--- Assets/npc/fred/FredNipples.cs
+++ Assets/npc/fred/FredNipples.cs
@@ -8,17 +8,13 @@ public class FredNipples : MonoBehaviour
     public Animator nipple_animator;
     public Item shirt;
     [SerializeField] bool shirt_on = true;
-    void Start()
-    {
-        nipple_animator = GetComponent<Animator>();
-    }
+    
 
     // Update is called once per frame
     void Update()
     {
-        if(Inventory.Instance.HasItem(shirt)){
-        shirt_on = !Inventory.Instance.has_shirt;
-        }
+        shirt_on = !Inventory.Instance.HasItem(shirt);
+     
 
         nipple_animator.SetBool("shirt", shirt_on);
     }
