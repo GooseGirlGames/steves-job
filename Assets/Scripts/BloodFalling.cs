a22404f49b9b16dfbe2ec4d1f8f8a0ae102ed7e1
Mielein <marie.trojan@uni-weimar.de>
2021-05-17 16:16:38 +0200

    commit for main pull

--- Assets/Scripts/BloodFalling.cs
+++ Assets/Scripts/BloodFalling.cs
@@ -2,30 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-public class BloodFalling : MonoBehaviour
-{
+public class BloodFalling : MonoBehaviour {
+
     public Progressbar progressbar;
-
     Rigidbody2D rigidbody2D;
     public Sprite splatter;
     private Vector2 screenbounds;
-    void Start()
-    {
+
+    void Start() {
         //progressbar.minBlood(counter);
         //rigidbody2D = this.GetComponent<Rigidbody2D> ();
         screenbounds = Camera.main.ScreenToWorldPoint(transform.position);
     }
 
-    void onTriggerEnter2D (Collider2D collider2D)
-    {
+    void onTriggerEnter2D (Collider2D collider2D) {
         if (collider2D.gameObject.name.Equals("Player"))
         {
             rigidbody2D.isKinematic = false;
         }
     }
-    void OnCollisionEnter2D(Collision2D collision2D)
-    {
+
+    void OnCollisionEnter2D(Collision2D collision2D) {
         if(collision2D.gameObject.name.Equals("Background")){
             this.gameObject.GetComponent<SpriteRenderer>().sprite = splatter;
             this.gameObject.tag = "splatter";            
@@ -42,19 +41,23 @@ public class BloodFalling : MonoBehaviour
 
     }
 
-void OnEnable(){
+    void OnEnable(){
         GameObject[] objects = GameObject.FindGameObjectsWithTag("splatter");
         foreach(GameObject obj in objects){
             Physics2D.IgnoreCollision(obj.gameObject.GetComponent<Collider2D>(), this.GetComponent<Collider2D>(), true);
         }
     }
+
+    void NewScene(){
+        SceneManager.LoadScene("blood_lost");
+    }
+
     void Update(){
         GameObject[] objectsSplat = GameObject.FindGameObjectsWithTag("splatter");
         int countSplat = objectsSplat.Length;
-
         
-
         if(countSplat == 3){
+            NewScene();
             Debug.Log("Game Over");
         }
     }
