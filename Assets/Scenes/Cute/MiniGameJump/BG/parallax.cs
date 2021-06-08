7fe80d765d6dac7907ef50662148386b5c5a2895
Mielein <marie.trojan@uni-weimar.de>
2021-06-08 21:24:29 +0200

    parallex kinda works fine now

--- Assets/Scenes/Cute/MiniGameJump/BG/parallax.cs
+++ Assets/Scenes/Cute/MiniGameJump/BG/parallax.cs
@@ -9,14 +9,18 @@ public class parallax : MonoBehaviour
     public float parallaxEffect;
     void Start()
     {
-        startpos = transform.position.x;
+        startpos = camera.transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
+        float tmp = (camera.transform.position.x*(1-parallaxEffect));
         float distance = camera.transform.position.x*parallaxEffect;
         transform.position = new Vector3(startpos+distance, transform.position.y, transform.position.z);
+
+        if(tmp > startpos + length) startpos += length;
+        else if(tmp < startpos) startpos -= length;
     }
 }
