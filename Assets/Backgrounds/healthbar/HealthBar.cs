52c59ee2e7ce633a93a9a4b0a02ab84813ad3649
Mielein <marie.trojan@uni-weimar.de>
2021-06-23 09:07:55 +0200

    OMG I THOUGHT PARALLAX WAS DEAD BUT I NOW I REANIMATED IT

---
+++ Assets/Backgrounds/healthbar/HealthBar.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    private Transform bar;
+
+    private void Start()
+    {
+        bar = transform.Find("Bar");
+    }
+
+    public void SetSize(float sizeNormalized)
+    {
+        bar.localScale = new Vector3(sizeNormalized, 1f);
+    }
+}
