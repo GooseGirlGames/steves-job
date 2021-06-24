017b49453ef6488c33058c8b43dcd6e6c42bd857
Mielein <marie.trojan@uni-weimar.de>
2021-06-24 10:40:32 +0200

    just trying to get the miniGameStory

---
+++ Assets/Scenes/Minigames/MiniGameJump/CandyDialouge.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CandyDialouge : DialogueTrigger
+{   
+    public Portal portalToMiniGame;
+    public Item sweetDirt;
+    public Item mop;
+    public GameObject racoon;
+/*     public Transform player;
+    private float racoon_pos;
+    private float player_pos; */
+
+    public void Start()
+    {
+        Inventory.Instance.AddItem(mop);
+        racoon.SetActive(false);
+    }
+
+    public void racoonActivated(){
+        racoon.SetActive(true);
+/*         racoon_pos = racoon.transform.position.x;
+        player_pos = player.position.x;
+        while(racoon_pos != player_pos - 3.0f){
+            racoon.transform.position.x = racoon_pos + 0.1f;
+        } */
+
+    }
+
+    public void floorCleaned(){
+        Inventory.Instance.AddItem(sweetDirt);
+        racoonActivated();
+        Destroy(this.gameObject);
+    }
+
+    public Dialogue hasMopDia;
+    public Dialogue needMopDia;
+
+    public override Dialogue GetActiveDialogue() {
+        if (Inventory.Instance.HasItem(mop)) {
+            return hasMopDia;
+            
+        }
+       return needMopDia; 
+    }
+}
