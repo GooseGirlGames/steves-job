8a31518f9d14a4cb849f887240f5733665c1cd29
YoFionn <fionn.edward.erickson@gmail.com>
2021-06-24 18:10:30 +0200

    Itemmanager in Gamemanager and created

---
+++ Assets/Dialogue/ItemManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Dictionary<string, Item> item_index;
+ 
+    public Item shirt;
+    public Item shirt_dirty;
+    public Item shirt_clean;
+    public Item bucked;
+    public Item bloody_bucked;
+    public Item Mop;
+}
