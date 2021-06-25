b47d35c1e879d9e0879e12806d0e47a1713399ff
Jakob Ruckel <jakob@ruckel.de>
2021-06-26 00:06:03 +0200

    Fix actionBox selection

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,11 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance = null;
     public InteractionHintUI hintUI;
+    public EventSystem EventSystem;
 
     private void Awake() {
         if (Instance != null) {
