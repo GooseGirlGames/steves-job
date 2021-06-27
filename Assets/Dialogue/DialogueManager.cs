915ec492707df8dfc1000a3a7455d85c0d65c514
Jakob Ruckel <jakob@ruckel.de>
2021-06-27 22:46:37 +0200

    Failed attempt to fix re-triggering issue

--- Assets/Dialogue/DialogueManager.cs
+++ Assets/Dialogue/DialogueManager.cs
@@ -38,6 +38,7 @@ public class DialogueManager : MonoBehaviour
 
     public float lastKeyPress = -1.0f;
     public const float KEY_PRESS_TIME_DELTA = 0.3f;  // seconds
+    public const float NEW_DIALOGUE_START_COOLDOWN = 1.0f;  // prevents option selection immediately re-triggering dialogue
     private const string DIALOGUE_LOCK_TAG = "DialogueManager";
 
     public static void Log(string msg) {
@@ -278,8 +279,10 @@ public class DialogueManager : MonoBehaviour
     }
 
     private void DialogueEnded() {
+        lastKeyPress += NEW_DIALOGUE_START_COOLDOWN;
         DialogueManager.Log("Dialog Ended");
         dialogueCanvas.enabled = false;
+        //canBeAdvancedByKeypress = true;
         activeDialogue = null;
         currentSentence = null;
         currentTrigger = null;
@@ -295,16 +298,20 @@ public class DialogueManager : MonoBehaviour
 
     private void Update() {
         if (!IsDialogueActive()) {
+            // Set anyway to avoid re-triggering a dialogue when a dialogue is exited 
+            // because an option was chosen.  Also see #67.
+            // TODO if (Input.GetKeyDown(DIALOGUE_KEY)) lastKeyPress = Time.fixedTime;
             return;
         }
         
-        if (Time.fixedTime - lastKeyPress < KEY_PRESS_TIME_DELTA) {
-            //DialogueManager.Log("Too fast " + Time.fixedTime + ", " + lastKeyPress);
-            return;
-        }
-        if (canBeAdvancedByKeypress && Input.GetKeyDown(DIALOGUE_KEY)) {
+        bool tooFast = (Time.fixedTime - lastKeyPress) < KEY_PRESS_TIME_DELTA;
+        
+        if (Input.GetKeyDown(DIALOGUE_KEY)) {
+            DialogueManager.Log("E Pressed!");
             lastKeyPress = Time.fixedTime;
-            DisplayNextSentence();
+            if (!tooFast && canBeAdvancedByKeypress) {
+                DisplayNextSentence();
+            }
         }
     }
 
