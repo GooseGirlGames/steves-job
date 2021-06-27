19620f178db41a07d2316107cee6eace4804f4f1
Mielein <marie.trojan@uni-weimar.de>
2021-06-27 20:18:46 +0200

    working on positioning the spawns

---
+++ Assets/Scenes/Minigames/MiniGameJump/RacoonHit.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RacoonHit : MonoBehaviour
+{
+    public bool hit = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.tag == "spawn"){
+            hit = true;
+            Debug.Log("hit");
+        }
+    }
+
+}
