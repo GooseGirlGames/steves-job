5d5e3b3663f9cf05842f085390822cc86461b701
Mielein <marie.trojan@uni-weimar.de>
2021-06-30 12:13:26 +0200

    Jimmy Dialogue Script test

--- Assets/NPC/jimmy/JimmyDialogue.cs
+++ Assets/NPC/jimmy/JimmyDialogue.cs
@@ -5,8 +5,14 @@ using UnityEngine;
 public class JimmyDialogue : DialogueTrigger
 {
     public Portal portalToMiniGame;
+    public Item shirt;
+    public Item clean_shirt;
+    public Item dirty_shirt;
     public Item bucket;
     public Item empty;
+    public bool gameLost = false;
+    public bool gamePlayed = false;
+    public static new JimmyDialogue Instance = null;
 
 
     public void EnterMiniGame() {
@@ -14,40 +20,115 @@ public class JimmyDialogue : DialogueTrigger
     }
 
     public override Dialogue GetActiveDialogue() {
+        if (gamePlayed) {
+            return new NoBigAnswer();
+        }
+        else if (gameLost){
+            return new JimmyLoseDialogue();
+        }
         if (Inventory.Instance.HasItem(bucket)) {
             return new JimmyWinDialogue();
         }
         else if (Inventory.Instance.HasItem(empty)){
-            return new JimmyLoseDialogue();
+            return new JimmyHasBucketDialogue();
         }
         return new JimmyDefaultDialogue();
+    }
+
+    private void Awake() {
+        Instance = this;
     }
 }
 
 public class JimmyDefaultDialogue : Dialogue {
     public JimmyDefaultDialogue() {
-
-        JimmyDialogue diaTrigger = (JimmyDialogue) JimmyDialogue.Instance;
-
         Say("Hey there, Janitor!");
+        Say("Pipes are busted, can ya help?")
+            .Choice(new TextOption("Yes")
+                .IfChosen(new TriggerDialogueAction<BucketMissing>()))
+            .Choice(new TextOption("No"));
+    }
+}
 
+public class JimmyHasBucketDialogue : Dialogue {
+    public JimmyHasBucketDialogue() {
+        Say("Hey there, Janitor!");
         Say("Pipes are busted, can ya help?")
             .Choice(
                 new TextOption("Yes")
                 .IfChosen(new DialogueAction(() => {
-                    diaTrigger.EnterMiniGame();
+                    JimmyDialogue.Instance.EnterMiniGame();
                 }))
             )
             .Choice(new TextOption("No"));
-
     }
 }
 
+public class BucketMissing : Dialogue {
+    public BucketMissing() {
+        Say("ummm..I think you need a bucket for this");
+        Say("You should look for one");
+    }
+}
 
+public class CantDoAnythingWithIt : Dialogue {
+    public CantDoAnythingWithIt() {
+        Say("uhhh...this...uhh..well that is awkward");
+        Say("I just dont know what to do with it");
+    }
+}
+/*----------------------- Game has been won --------------------------------------------*/
+public class NoBigAnswer : Dialogue {
+    public NoBigAnswer() {
+        Say("What's your problem?");
+        Say("You have something to clean?")
+        .Choice(new ItemOption(JimmyDialogue.Instance.empty)
+            .IfChosen(GiveItem(JimmyDialogue.Instance.bucket))
+            .IfChosen(new TriggerDialogueAction<BucketFilledUp>()))
+        .Choice(new ItemOption(JimmyDialogue.Instance.bucket)
+            .IfChosen(new TriggerDialogueAction<NothingToDoHere>()))
+        .Choice(new ItemOption(JimmyDialogue.Instance.clean_shirt)
+            .IfChosen(new TriggerDialogueAction<CleanShirtDia>()))
+        .Choice(new ItemOption(JimmyDialogue.Instance.shirt)
+            .IfChosen(new TriggerDialogueAction<CleanShirtDia>()))
+        .Choice(new ItemOption(JimmyDialogue.Instance.dirty_shirt)
+            .IfChosen(new TriggerDialogueAction<DirtyShirtDia>()))
+        .Choice(new OtherItemOption()
+            .IfChosen(new TriggerDialogueAction<CantDoAnythingWithIt>()));  
+    }
+}
+/*----------------------- Shirt Dialogue --------------------------------------------*/
+public class CleanShirtDia : Dialogue {
+    public CleanShirtDia() {
+        Say("urgh");
+        Say("this looks disgusting!!!");
+        Say("Let me clean that for you")
+            .DoAfter(GiveItem(JimmyDialogue.Instance.dirty_shirt));
+    }
+}
+public class DirtyShirtDia : Dialogue {
+    public DirtyShirtDia() {
+        Say("looks completely fine to me");
+        Say("I dont know what you want me to do with that");
+    }
+}
+/*----------------------- Bucket Dialogue ----------------------------------------------------------*/
+public class BucketFilledUp : Dialogue {
+    public BucketFilledUp() {
+        Say("There you go");
+        Say("Your bucket is all filled up again");
+    }
+}
+public class NothingToDoHere : Dialogue{
+    public NothingToDoHere() {
+        Say("Huh...?");
+        Say("All good with that?");
+        Say("Give me something to work with");
+    }
+}
+/*----------------------- Game still needs to be played --------------------------------------------*/
 public class JimmyWinDialogue : Dialogue {
     public JimmyWinDialogue() {
-        
-
         Say("Excellent!")
             .If(() => 
                 BloodFalling.splatCount == 0
@@ -58,9 +139,11 @@ public class JimmyWinDialogue : Dialogue {
                 BloodFalling.splatCount == 2
             );
 
-        Say("Thank for the help!");
+        Say("Thank for the help!")
+            .Do(() => {JimmyDialogue.Instance.gamePlayed = true;});
     }
 }
+
 
 
 public class JimmyLoseDialogue : Dialogue {
@@ -75,8 +158,10 @@ public class JimmyLoseDialogue : Dialogue {
                 new TextOption("Yes")
                 .IfChosen(new DialogueAction(() => {
                     diaTrigger.EnterMiniGame();
-                }))
-            )
-            .Choice(new TextOption("Later"));
+                })))
+            .Choice(new TextOption("Later")
+            .IfChosen(new DialogueAction(() => {
+                    JimmyDialogue.Instance.gameLost = true;
+                })));
     }
 }
