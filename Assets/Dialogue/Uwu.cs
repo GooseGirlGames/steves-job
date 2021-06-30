c2844af4c238e64d046934a11482ec2b0cc22e8a
Jakob Ruckel <jakob@ruckel.de>
2021-06-30 11:54:19 +0200

    uwu: Also uwufy capitalized words, re-order

--- Assets/Dialogue/Uwu.cs
+++ Assets/Dialogue/Uwu.cs
@@ -8,6 +8,20 @@ public class Uwu {
     }
     public static string Uwufy(string text) {
         return text
+            .Replace("small", "smol")
+            .Replace("cute", "kawaii")
+            .Replace("fluff", "floof")
+            .Replace("love", "luv")
+            .Replace("stupid", "baka")
+            .Replace("what", "nani")
+            .Replace("meow", "nya")
+            .Replace("Small", "Smol")
+            .Replace("Cute", "Kawaii")
+            .Replace("Fluff", "Floof")
+            .Replace("Love", "Luv")
+            .Replace("Stupid", "Baka")
+            .Replace("What", "Nani")
+            .Replace("Meow", "Nya")
             .Replace("r", "w")
             .Replace("R", "W")
             .Replace("l", "w")
@@ -15,14 +29,7 @@ public class Uwu {
             .Replace("no", "nyo")
             .Replace("na", "nya")
             .Replace("No", "Nyo")
-            .Replace("Na", "Nya")
-            .Replace("small", "smol")
-            .Replace("cute", "kawaii")
-            .Replace("fluff", "floof")
-            .Replace("love", "luv")
-            .Replace("stupid", "baka")
-            .Replace("what", "nani")
-            .Replace("meow", "nya");
+            .Replace("Na", "Nya");
     }
         
 }
