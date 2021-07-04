82cb2de2ddf18dbe6ee8f86856a82b53216a2fe7
Jakob Ruckel <jakob@ruckel.de>
2021-07-05 00:22:38 +0200

    Menu: Fix no button slected in builds

--- Assets/Menu/StartButtonPreSelect.cs
+++ Assets/Menu/StartButtonPreSelect.cs
@@ -2,11 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class StartButtonPreSelect : MonoBehaviour
 {
     public Button button;
-    private void Awake() {
+    private void Start() {
         StartCoroutine(UIUtility.SelectButtonLater(button));
+    }
+    private void Update() {
+        if (EventSystem.current.currentSelectedGameObject == null) {
+            StartCoroutine(UIUtility.SelectButtonLater(button));
+        }
     }
 }
