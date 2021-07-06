034f1aaf6a0fbdef432511abecfc301f0bdc8d8e
YoFionn <ericksoncooperation@gmail.com>
2021-07-07 01:17:54 +0200

    Vending-Dialogue.cs not done but there

--- Assets/NPC/void/Switch/SwitchDialogue.cs
+++ Assets/NPC/void/Switch/SwitchDialogue.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchDialogue : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+/* 
+Dialogue1(First):
+    - The Cracked Remains of The switch,
+    - Get brocken dirty switch
+Dialogue2(No Power and Empty):
+    - Trigger No Steve E Finished Item and broken Switch or dirty Switch or brokendirty switch or switch in inventory
+    - the empty gap without switch
+    - can accept switch
+Dialogue3(No Power):
+    - No Power here but switch(nothing happening if used)
+Dialogue(Empty):
+    - Dangerous, sparks are flying ect ect
+    - Switch can be placed and afterwards activadet
+Dialogue(Final):
+    - Switch can be pressed
+ */
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class SwitchDialogue : DialogueTrigger{
+
+    public Item;
+    public Item;
+    public Item;
+
+    public override Dialogue GetActiveDialogue() {
+        if (Inventory.Instance.CoinBalance() == 0) {
+            return new NoCoins();
+        }
+
+        if (!Inventory.Instance.HasItem(item_for_sale)) {
+            return new BuyItem(this);
+        } else {
+            return new ItemBought();
+        }
     }
 }
--- Assets/NPC/void/VendingMachine/VendingDialogue.cs
+++ Assets/NPC/void/VendingMachine/VendingDialogue.cs
@@ -2,17 +2,81 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VendingDialogue : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class VendingDialogue : DialogueTrigger{
+    public const string VENDINGMACHINE_NAME = "AQUACLAW";
+    public Item item_for_sale;
+    public Item startcoin;
+    public Item cutecoin;
+    public Item horrorcoin;
+
+    private void Awake() {
+        this.name = VENDINGMACHINE_NAME;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public override Dialogue GetActiveDialogue() {
+        if (Inventory.Instance.CoinBalance() == 0) {
+            return new NoCoins();
+        }
+
+        if (!Inventory.Instance.HasItem(item_for_sale)) {
+            return new BuyItem(this);
+        } else {
+            return new ItemBought();
+        }
     }
+
+    public class BuyItem : Dialogue {
+        public BuyItem(VendingDialogue t) {
+            EmptySentence().Do(new TriggerDialogueAction<Welcome>());
+
+            Say("BuyItem?")
+            .Choice(
+                new ItemOption(t.startcoin)
+                .IfChosen(RemoveItem(t.startcoin))
+                .IfChosen(GiveItem(t.item_for_sale))
+            )
+            .Choice(
+                new ItemOption(t.horrorcoin)
+                .IfChosen(RemoveItem(t.horrorcoin))
+                .IfChosen(GiveItem(t.item_for_sale))
+            )
+            .Choice(
+                new ItemOption(t.cutecoin)
+                .IfChosen(RemoveItem(t.cutecoin))
+                .IfChosen(GiveItem(t.item_for_sale))
+            )
+            .Choice(
+                new TextOption("No")
+            )
+            .DoAfter(new TriggerDialogueAction<Bye>());
+        }
+    }
+
+
+    public class ItemBought : Dialogue {
+        public ItemBought() {
+            Say("ItemBought");
+        }
+    }
+
+
+    public class NoCoins : Dialogue {
+        public NoCoins() {
+            Say("NoCoins");
+        }
+    }
+
+    public class Welcome : Dialogue {
+        public Welcome() {
+            Say("Welcome to the " + VENDINGMACHINE_NAME + "!");
+        }
+    }
+
+    public class Bye : Dialogue {
+        public Bye() {
+            Say("Thank you for using " + VENDINGMACHINE_NAME + "!");
+        }
+    }
+
+
 }
