f58c1c1e543a04072ebda47520fc5479d4935338
Jakob Ruckel <jakob@ruckel.de>
2021-07-10 20:39:54 +0200

    Finish up Void Jim dialogue

--- Assets/NPC/void/Jim/JimDialogue.cs
+++ Assets/NPC/void/Jim/JimDialogue.cs
@@ -5,13 +5,12 @@ using UnityEngine;
 /* 
 
  */
-public class JimDialogue : DialogueTrigger{
-    public Item _jim_horror_finished;
-    public Item _jim_cute_finished;
-    public Item _firstDialogueFinished;
+public class JimDialogue : VoidNPCDiaTrigger {
+    public Item _said_thanks;
     public Item _hasCleanedSwitch;
     public Item brokenSwitch;
-    public Item dirtySwitch;
+    public Item cuteBrokenSwitch;
+    public Item horrorBrokenSwitch;
     public Item dirtyBrokenSwitch;
     public Item finishedSwitch;
     public Item bucket;
@@ -22,109 +21,151 @@ public class JimDialogue : DialogueTrigger{
     public Item bloodybowgoose;
     public Item marysPeriod;
     public static JimDialogue t;
+    public override void UpdateStaticT() {
+        t = this;
+    }
 
-    public override Dialogue GetActiveDialogue() {
-        JimDialogue.t = this;
-        if(Inventory.Instance.HasItem(_jim_cute_finished)&&Inventory.Instance.HasItem(_jim_horror_finished)){
-            if (Inventory.Instance.HasItem(_hasCleanedSwitch)){
+
+    public override Dialogue NewFullRestoredDia() {
+        if (!Inventory.Instance.HasItem(_said_thanks)) {
+            return new Thanks();
+        } else {
+            if (Inventory.Instance.HasItem(_hasCleanedSwitch)) {
                 return new IsNowUseless();
             }
-            else if(Inventory.Instance.HasItem(_firstDialogueFinished)){
-                return new DefaultDialogue();
-            }
-            return new RestoredDialogue();
+            return new CleanItem();
         }
-        else if(Inventory.Instance.HasItem(_jim_cute_finished)||Inventory.Instance.HasItem(_jim_horror_finished)){
-            return new HalfExist();
-        }
-        return new NoneExist();
     }
-    public class NoneExist : Dialogue{
-        public NoneExist(){
+    public override Dialogue NewHalfRestoredDia() => new HalfRestoredDia();
+    public override Dialogue NewGoneDia() => new GoneDia();
+
+
+    public class GoneDia : Dialogue {
+        public GoneDia() {
             Say("...");
         }
     }
-    public class HalfExist : Dialogue{
-        public HalfExist(){
-            Say("Ah I dont feel so good ... its like my atoms are not all with me...");
+    public class HalfRestoredDia : Dialogue {
+        public HalfRestoredDia() {
+            Say("Mr. Steve, I don't feel so good... It's like not all of my atoms are with me...");
         }
     }
-    public class RestoredDialogue : Dialogue{
-        public RestoredDialogue(){
-            Say("Thanks Janitor, I think I was a little muddle-headed ... I think");
+    public class Thanks : Dialogue {
+        public Thanks(){
+            Say("Thanks Janitor, I think I was a little muddle-headed... I think.");
             Say("...");
-            Say("Anyway now im back");
-            Say("Well... At least in what is left of the mall");
-            Say("If I can help your with anything, just ask, My store is back in buissnes")
-                .DoAfter(new TriggerDialogueAction<DefaultDialogue>());
+            Say("Anyway, now I'm back.");
+            Say("Well... At least in what is left of the mall.");
+            Say("If I can help your with anything, just ask. My store is back in business.")
+            .Do(GiveItem(t._said_thanks));
         }
     }
-    public class DefaultDialogue : Dialogue{
-        public DefaultDialogue(){
-            Say("Should I clean something for you? and I mean for real this time, haha")
+    public class CleanItem : Dialogue {
+        public CleanItem() {
+            // Newlines within the same stetement, sue me
+            Say("Should I clean something for you? And I mean for real this time, haha")
                 .Choice(new TextOption("No Thanks"))
+
                 .Choice(new ItemOption(t.dirtyBrokenSwitch)
                     .IfChosen(new TriggerDialogueAction<DirtyBrokenSwitchDialogue>()))
+                .Choice(new ItemOption(t.cuteBrokenSwitch)
+                    .IfChosen(new TriggerDialogueAction<DirtyBrokenSwitchDialogue>()))
+                .Choice(new ItemOption(t.horrorBrokenSwitch)
+                    .IfChosen(new TriggerDialogueAction<DirtyBrokenSwitchDialogue>()))
+
                 .Choice(new ItemOption(t.finishedSwitch)
                     .IfChosen(new TriggerDialogueAction<SwitchDialogue>()))
+
                 .Choice(new ItemOption(t.brokenSwitch)
                     .IfChosen(new TriggerDialogueAction<BrokenSwitchDialogue>()))
+
                 .Choice(new ItemOption(t.fullbucket)
                     .IfChosen(new TriggerDialogueAction<FullBucketDialogue>()))
+
                 .Choice(new ItemOption(t.bloodygoose)
                     .IfChosen(new TriggerDialogueAction<GooseDialogue>()))
+
                 .Choice(new ItemOption(t.marysPeriod)
-                    .IfChosen(new TriggerDialogueAction<BloodyMaryDialogue>()));
+                    .IfChosen(new TriggerDialogueAction<BloodyMaryDialogue>()))
+                    
+                .Choice(new OtherItemOption()
+                    .IfChosen(new TriggerDialogueAction<OtherItemCleaning>()));
         }
     }
     public class DirtyBrokenSwitchDialogue : Dialogue{
         public DirtyBrokenSwitchDialogue(){
-            Say("Oh I see");
-            Say("I'm pretty sure I can clean the parts for your so there down for reapir")
-                .DoAfter(GiveItem(t.brokenSwitch));
-            Say("But I myself am no got with repairing stuff");
+            World from = DialogueManager.Instance.currentItem.originWorld;
+
+            Say("Alright, let me see...")
+            .If(() => from == World.Void);
+
+            Say("Oof, that's one hell of a putrid smell, but...")
+            .If(() => from == World.Horror);
+
+            Say("Oh, that's a lot of sugar, but...")
+            .If(() => from == World.Cute);
+
+            Say("I'm pretty sure I can clean the parts for you, so they're ready to be repaired.");
+
+            Say("There you go, clean parts. But I myself am no good at repairing stuff.")
+                .Do(GiveItem(t.brokenSwitch))
+                .Do(GiveItem(t._hasCleanedSwitch));
         }
     }
     public class SwitchDialogue : Dialogue{
         public SwitchDialogue(){
-            Say("That thing looks perfectly fine to me");
-            Say("Quick put it to power and switch us out of this shithole, I miss my other laundry-machines");
-            Say("i think Steve E. should be able to help with that.");
+            Say("That thing looks perfectly fine to me.");
+            Say(
+                "Quick, put it to power and switch us out of this shithole, "
+                + "I miss my other laundry machines."
+            );
+            Say("I think Steve E. should be able to help you with that.");
         }
     }
     public class BrokenSwitchDialogue : Dialogue{
         public BrokenSwitchDialogue(){
-            Say("It is clean, sadly I can't do more");
-            Say("If you find a way to talk to Handyman, he could probably help your out");
-            Say("Good look, I belive in you")
-                .DoAfter(new TriggerDialogueAction<DefaultDialogue>());    
+            Say("It's clean, sadly I can't do more.");
+            Say("If you find a way to talk to Handyman, he'll probably help you out.");
+            Say("Good luck, I belive in you")
+                .DoAfter(new TriggerDialogueAction<CleanItem>());    
         }
     }
     public class BloodyMaryDialogue : Dialogue{
         public BloodyMaryDialogue(){
-            Say("Sorry but, No, no I really dont want to have something to do with that thing")
-                .DoAfter(new TriggerDialogueAction<DefaultDialogue>());
+            Say("Sorry but, no, no I really dont want to have something to do with that thing.")
+                .DoAfter(new TriggerDialogueAction<CleanItem>());
         }
     }
     public class FullBucketDialogue : Dialogue{
         public FullBucketDialogue(){
-            Say("Uff, yes this bucket is stinky, gimme a sec i clean it for you")
+            Say("Oof, yes this bucket is stinky. Gimme a sec, I'll clean it for you.")
                 .DoAfter(GiveItem(t.bucket));
             Say("here you go");
         }
     }
-    public class GooseDialogue : Dialogue{
+    public class GooseDialogue : Dialogue {
         public GooseDialogue(){
-            Say("oh the poor thing, wait let me polish him for you");
-            Say("here you go little one")
+            Say("Oh the poor thing, wait let me polish her for you.");
+            Say("Here you go, little one.")
                 .DoAfter(GiveItem(t.goose))
-                .DoAfter(new TriggerDialogueAction<DefaultDialogue>());
+                .DoAfter(new TriggerDialogueAction<CleanItem>());
         }
     }
-    public class IsNowUseless : Dialogue{
-        public IsNowUseless(){
-            Say("I think the most important thing I already cleaned for you, but I can still do something for your if your like")
-                .DoAfter(new TriggerDialogueAction<DefaultDialogue>());
+    public class IsNowUseless : Dialogue {
+        public IsNowUseless() {
+            Say(
+                "I think the most important thing I already cleaned for you, "
+                + "but I can still do something for your if you like."
+            )
+            .DoAfter(new TriggerDialogueAction<CleanItem>());
+        }
+    }
+
+    public class OtherItemCleaning : Dialogue {
+        public OtherItemCleaning() {
+            string item = DialogueManager.Instance.currentItem.name;
+            Say("Umm, I'm terribly sorry, but I really don't know how to clean a " + item + ".")
+            .DoAfter(new TriggerDialogueAction<CleanItem>());
         }
     }
 }
