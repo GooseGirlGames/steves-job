656d3f4b1b5b9d4ea574cb1038888af063509d03
Jakob Ruckel <jakob@ruckel.de>
2021-07-11 01:55:38 +0200

    JumpMiniGame: Fix cam for now

--- Assets/Camera/FindPlayer.cs
+++ Assets/Camera/FindPlayer.cs
@@ -5,7 +5,9 @@ using Cinemachine;
 
 public class FindPlayer : MonoBehaviour
 {
+    public bool enable = true;
     void Awake() {
+        if (!enable) return;
         GameObject f_cam = GameObject.Find("CameraFixed");
         CinemachineVirtualCamera v_cam = GetComponent<CinemachineVirtualCamera>();
         v_cam.Follow = f_cam.transform;
