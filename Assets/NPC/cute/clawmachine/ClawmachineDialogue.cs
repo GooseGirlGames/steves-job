397dcc969bc7eb2c38545cb026b4d4007eaa180e
Jakob Ruckel <jakob@ruckel.de>
2021-07-11 14:59:48 +0200

    Add Clawmachine animation

--- Assets/NPC/cute/clawmachine/ClawmachineDialogue.cs
+++ Assets/NPC/cute/clawmachine/ClawmachineDialogue.cs
@@ -10,15 +10,25 @@ public class ClawmachineDialogue : DialogueTrigger {
     public Item cutecoin;
     public Item horrorcoin;
     public Item machine_used;
+    public List<Animator> animators;
+    private void SetAnimatorTrigger(string trigger) {
+        foreach (var a in animators) a.SetTrigger(trigger);
+    }
+    public Sprite avatar_before;
+    public Sprite avatar_after;
 
     private void Awake() {
         this.name = CLAWMACHINE_NAME;
+        GetActiveDialogue();  // Set animation triggers
     }
 
     public override Dialogue GetActiveDialogue() {
         ClawmachineDialogue.t = this;
+        avatar = avatar_before;
 
         if (Inventory.Instance.HasItem(machine_used)) {
+            t.SetAnimatorTrigger("Empty");
+            avatar = avatar_after;
             return new Bye();
         }
 
@@ -26,7 +36,7 @@ public class ClawmachineDialogue : DialogueTrigger {
             return new NoCoins();
         }
 
-        return new BuyItem();
+        return new Welcome();
     }
 
     public class BuyItem : Dialogue {
@@ -35,22 +45,28 @@ public class ClawmachineDialogue : DialogueTrigger {
 
             EmptySentence().Do(new TriggerDialogueAction<Welcome>());
 
-            Say("BuyItem?")
+            Say(
+                "Want to give the amazing, guaranteed to not be cursed "
+                + CLAWMACHINE_NAME + " a try?"
+                )
             .Choice(
                 new ItemOption(t.startcoin)
                 .IfChosen(RemoveItem(t.startcoin))
+                .IfChosen(new DialogueAction(() => t.SetAnimatorTrigger("Dispense")))
                 .IfChosen(GiveItem(t.item_for_sale))
                 .IfChosen(GiveItem(t.machine_used))
             )
             .Choice(
                 new ItemOption(t.horrorcoin)
                 .IfChosen(RemoveItem(t.horrorcoin))
+                .IfChosen(new DialogueAction(() => t.SetAnimatorTrigger("Dispense")))
                 .IfChosen(GiveItem(t.item_for_sale))
                 .IfChosen(GiveItem(t.machine_used))
             )
             .Choice(
                 new ItemOption(t.cutecoin)
                 .IfChosen(RemoveItem(t.cutecoin))
+                .IfChosen(new DialogueAction(() => t.SetAnimatorTrigger("Dispense")))
                 .IfChosen(GiveItem(t.item_for_sale))
                 .IfChosen(GiveItem(t.machine_used))
             )
@@ -73,13 +89,15 @@ public class ClawmachineDialogue : DialogueTrigger {
 
     public class Welcome : Dialogue {
         public Welcome() {
-            Say("Welcome to the " + CLAWMACHINE_NAME + "!");
+            Say("Welcome to the " + CLAWMACHINE_NAME + "!")
+            .DoAfter(new TriggerDialogueAction<BuyItem>());
         }
     }
 
     public class Bye : Dialogue {
         public Bye() {
             ClawmachineDialogue t = ClawmachineDialogue.t;
+            t.avatar = t.avatar_after;
 
             Say("Thank you for using " + CLAWMACHINE_NAME + "!");
 
