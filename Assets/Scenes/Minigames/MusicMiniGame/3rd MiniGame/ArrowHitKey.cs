5c45fe4fe035569c28168ec83d71f6e9a17b7450
Mielein <marie.trojan@uni-weimar.de>
2021-07-11 16:24:09 +0200

    hit or miss i guess they never miss huh

--- Assets/Scenes/Minigames/MusicMiniGame/3rd MiniGame/ArrowHitKey.cs
+++ Assets/Scenes/Minigames/MusicMiniGame/3rd MiniGame/ArrowHitKey.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class ArrowHitKey : MonoBehaviour
 {
     public bool pressable;
+    public bool hit = false;
+    public bool missed = false;
     public KeyCode keyToPress;
     void Start(){
         
@@ -13,10 +15,21 @@ public class ArrowHitKey : MonoBehaviour
     void Update(){
         if(Input.GetKeyDown(keyToPress)){
             if(pressable){
+                hit = true;
                 gameObject.SetActive(false);
+                
+            }
+            if(hit){
                 MiniGameManager.instance.NoteHit();
             }
+            
         }
+        if(pressable && !hit){
+            if(!missed){
+                missed = true; 
+                MiniGameManager.instance.NoteMissed();
+            }   
+        }       
     }
 
     void OnTriggerEnter2D(Collider2D other){
@@ -27,7 +40,6 @@ public class ArrowHitKey : MonoBehaviour
     void OnTriggerExit2D(Collider2D other){
         if(other.tag == "button"){
             pressable = false;
-            MiniGameManager.instance.NoteMissed();
         }
     }
 }
