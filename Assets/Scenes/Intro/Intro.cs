c90e03a257bda05fa78d117cbb29a1a1e4aad4ee
Jakob Ruckel <jakob@ruckel.de>
2021-07-12 20:35:24 +0200

    Fix up Intro text

--- Assets/Scenes/Intro/Intro.cs
+++ Assets/Scenes/Intro/Intro.cs
@@ -11,12 +11,12 @@ public class Intro : MonoBehaviour {
     private List<string> texts = new List<string>{
         "You're Steve, working as a janitor at an ordinary mall.",
         "After one particularly long day of work,",
-        "just as you want off the main lights to the mall...",
-        "...you flick the disused, old switch next to the light switch.",
+        "just as you want to turn off the main lights to the mall...",
+        "...you flick the old, never-used switch next to the light switch.",
         "That switch must have gone unused for decades...",
         "...but only now do you notice the yellowed printout above it:",
         "\"DO NOT TURN OFF\n                                            \nHOLDS REALITY TOGETHER\"",
-        "                        \nOops..."
+        "                    \nOops..."
     };
 
     void Start() {
