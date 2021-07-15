816b79176ca299e4040020e96f1eaf94231c12eb
Jakob Ruckel <jakob@ruckel.de>
2021-07-15 22:14:26 +0200

    Add DDR avatars

--- Assets/NPC/void/DDRMachine/DdrDialogue.cs
+++ Assets/NPC/void/DDRMachine/DdrDialogue.cs
@@ -9,11 +9,28 @@ public class DdrDialogue : DialogueTrigger {
     public Portal minigamePortal;
     public static DdrDialogue t = null;
 
+    public Sprite ava_gone;
+    public Sprite ava_half;
+    public Sprite ava_full;
+    public Item _r_cute;
+    public Item _r_horr;
+
+    private void LateUpdate() {
+        // Note: Animations are handeled through Steve-E-Wonder's list of additional animators
+        int state = (Inventory.Instance.HasItem(_r_cute) ? 1 : 0)
+                  + (Inventory.Instance.HasItem(_r_horr) ? 1 : 0);
+        if (state == 0)
+            avatar = ava_gone;
+        if (state == 1)
+            avatar = ava_half;
+        if (state == 2)
+            avatar = ava_full;
+    }
 
     public override Dialogue GetActiveDialogue() {
         t = this;
         if(Inventory.Instance.HasItem(_can_use_ddr)){
-        return new IntroDia();
+            return new IntroDia();
         }
         if(Inventory.Instance.HasItem(_powered)){
             return new GgDia();
