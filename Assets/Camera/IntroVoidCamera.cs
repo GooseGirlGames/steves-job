e69bee7a8f8738345e9f4809ddc394fdff4ccbe9
Jakob Ruckel <jakob@ruckel.de>
2021-07-17 14:12:43 +0200

    Only trigger IntroVoidCamera *once*

--- Assets/Camera/IntroVoidCamera.cs
+++ Assets/Camera/IntroVoidCamera.cs
@@ -3,20 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 
-public class IntroVoidCamera : MonoBehaviour
-{
-    private static bool triggered = false;
+public class IntroVoidCamera : MonoBehaviour {
+    public Item _switch_not_picked_up;
     /**
      * Set the camera's priority to -10 after a delay.
      */
     IEnumerator LowPriorityWithDelay(float seconds) {
-        IntroVoidCamera.triggered = true;
         yield return new WaitForSeconds(seconds);
         SetPriority(-10);
     }
 
     void Awake() {
-        if (!triggered) {
+        if (Inventory.Instance.HasItem(_switch_not_picked_up)) {
             SetPriority(100);
             StartCoroutine(LowPriorityWithDelay(2.0f));
         }
