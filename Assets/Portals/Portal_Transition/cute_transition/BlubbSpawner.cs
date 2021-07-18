545e58496019bb708213314cdb11dc0d19636976
Jakob Ruckel <jakob@ruckel.de>
2021-07-18 16:11:27 +0200

    Fix boxy bubble spawns at high res
    Fixes #202

--- Assets/Portals/Portal_Transition/cute_transition/BlubbSpawner.cs
+++ Assets/Portals/Portal_Transition/cute_transition/BlubbSpawner.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class BlubbSpawner : MonoBehaviour {
-    public const float MAX_COUNT = 643;
+    public const float MAX_COUNT = 343;
     public GameObject bubblePrefab;
+    public RectTransform rect1;
+    public RectTransform rect2;
 
     public void SpawnBubbles() {
         for (int i = 0; i < MAX_COUNT; ++i) {
-            Instantiate(bubblePrefab, parent: this.gameObject.transform);
+            Blubb b = Instantiate(bubblePrefab, parent: this.gameObject.transform).GetComponent<Blubb>();
+            b.BubbleStart(rect1, rect2);
         }
     }
 
